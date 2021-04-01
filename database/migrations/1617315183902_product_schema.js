1aee9a420705aed4e7a370cb30c3b8868ccbfe9d
Victor Ribeiro de Lima <victorlima.12@hotmail.com>
2021-04-01 19:21:23 -0300

    creating table products and brands

---
+++ database/migrations/1617315183902_product_schema.js
@@ -0,0 +1,24 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class ProductSchema extends Schema {
+  up () {
+    this.create('products', (table) => {
+      table.increments()
+      table.string('name', 80).notNullable()
+      table.string('description', 250).notNullable()
+      table.float('price',6,2).notNullable().unsigned()
+      table.integer('stock_amount').notNullable().unsigned()
+      table.integer('brand_id').references('id').inTable('brands').unsigned()
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('products')
+  }
+}
+
+module.exports = ProductSchema
